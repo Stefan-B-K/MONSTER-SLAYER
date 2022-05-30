d813dc9034888e4a179b27cbe297e7079b0bb0d0
iStef <stefan_b_k@icloud.com>
2022-05-30 13:03:39 +0300

    Finish Special Attack/Heal and end-game message

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-function randomAttack(min, max) {
+function randomHealth (min, max) {
   return Math.floor((Math.random() * (max - min) + min))
 }
 
@@ -9,26 +9,55 @@ const app = Vue.createApp({
       playerHealthMax: 100,
       monsterHealth: 200,
       monsterHealthMax: 200,
+      currentRound: 0,
+      winner: null
     }
   },
-  watch: {},
+  watch: {
+    playerHealth (value) {
+      if (value <= 0 && this.monsterHealth <= 0) this.winner = 'draw'
+      else if (value <= 0) this.winner = 'monster'
+    },
+    monsterHealth (value) {
+      if (value <= 0 && this.playerHealth <= 0) this.winner = 'draw'
+      else if (value <= 0) this.winner = 'player'
+    }
+  },
   computed: {
-    playerHealthBar() {
+    playerHealthBar () {
+      if (this.playerHealth())
       return this.playerHealth / this.playerHealthMax * 100 + '%'
     },
-    monsterHealthBar() {
+    monsterHealthBar () {
       return this.monsterHealth / this.monsterHealthMax * 100 + '%'
     },
+    specialAttackDisabled () {
+      return this.currentRound % 3 !== 0
+    }
   },
   methods: {
     attackMonster () {
-      const attackValue = randomAttack(5, 12)
+      this.currentRound++
+      const attackValue = randomHealth(8, 20)
+      this.monsterHealth -= attackValue
+      this.attackPlayer()
+    },
+    specialAttackMonster () {
+      this.currentRound++
+      const attackValue = randomHealth(20, 35)
       this.monsterHealth -= attackValue
       this.attackPlayer()
     },
     attackPlayer () {
-      const attackValue = randomAttack(8, 16)
+      const attackValue = randomHealth(8, 20)
       this.playerHealth -= attackValue
+    },
+    healPlayer () {
+      this.currentRound++
+      const healValue = randomHealth(10, 20)
+      if (this.playerHealth + healValue < 100) this.playerHealth += healValue
+      else this.playerHealth = 100
+      this.attackPlayer()
     }
   }
 })
